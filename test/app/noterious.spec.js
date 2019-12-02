11ce6f2525e4a21b6885706f100edc0d86ee2293
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2019-12-02 10:02:37 -0300

    Challenge 1 Solution - Test

--- test/app/noterious.spec.js
+++ test/app/noterious.spec.js
@@ -2,6 +2,23 @@
 
 describe('Noterious: configuration', function () {
 
+  describe('Boards route', function () {
+    var $state,
+      $rootScope,
+      state = 'boards'
+    
+    beforeEach(inject(function(_$state_, $templateCache, _$rootScope_) {
+      $state = _$state_;
+      $rootScope = _$rootScope_;
+
+      $templateCache.put('app/boards/boards.tmpl.html', '');
+    }));
+
+    it('should respond to url', function () {
+      expect($state.href(state)).toEqual('#/boards');
+    });
+  });
+
   beforeEach(module('noterious'));
 
   describe('Constant: noterious firebase endpoint', function () {
